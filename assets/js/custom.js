8e6fbd5f576dccd6aa6f79cbab65a008849ca261
Dipesh Thapa <dipesh@naphix.com.au>
2023-01-04 13:11:45 +0545

    Featured Events templating and styling

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,27 @@
 var MyNamespace = {
-    sayHello: function () {
-        // add click event listener to button
+    scrollToTop: function () {
+        // add click event listener to button and scroll to top
         document.getElementById('scroll-to-top-button').addEventListener('click', function() {
          window.scrollTo({ top: 0, behavior: 'smooth' });
         });
   
     },
+    toggleCard: function(){
+        // Get the child element
+        $(function(){
+            $('.card-open').click(function(){
+                var $this = $(this);
+                $(this).parents('.featured-events__card').addClass('is-open');
+                 $("html, body").animate({ scrollTop: $this.parents('.featured-events__card').offset().top - 58}, 0);
+            });
+            $('.featured-events__card-close').click(function(){
+                var $this = $(this);
+                $(this).parents('.featured-events__card').removeClass('is-open');
+            });
+        });
+
+    }
 };
 
-MyNamespace.sayHello();
+MyNamespace.scrollToTop();
+MyNamespace.toggleCard();
