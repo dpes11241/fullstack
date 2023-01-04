3a1d4b8316cfba1b312c7ed3e6330b42143fec76
Dipesh Thapa <dipesh@naphix.com.au>
2023-01-04 10:55:59 +0545

    Banner ScrolltoTop

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,11 @@
-console.log('Js ready');
+var MyNamespace = {
+    sayHello: function () {
+        // add click event listener to button
+        document.getElementById('scroll-to-top-button').addEventListener('click', function() {
+         window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+  
+    },
+};
+
+MyNamespace.sayHello();
